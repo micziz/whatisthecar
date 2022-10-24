7ff64fb2132610abd0f6bae27d722050b6c2c9f5
micziz <miczicontent@gmail.com>
2022-10-24 21:54:18 +0200

    Finish tut

---
+++ src/Main.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Home from "./pages/Home.svelte";
+  import Normal from "./pages/Normal.svelte";
+  import Htp from "./pages/HTP.svelte";
+  
+  import Router from 'svelte-spa-router'
+
+
+  const routes = {
+    // Exact path
+    '/': Home,
+    '/Normal': Normal,
+    '/htp': Htp
+  }
+</script>
+
+<main> 
+  <Router {routes}/>
+</main>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,52 @@
+<script lang="ts"> 
+    function goToNormal(){
+        window.location.href = '/#/normal'
+    }
+
+    function goToHTP(){
+        window.location.href = '/#/htp'
+    }
+</script>
+  
+<main> 
+    <h1 id="title" class="title is-1">What is the car</h1>
+
+    <h1 style="text-align: center;">Creato da Micziz</h1>
+
+    <div id="btn">
+        <button class="button is-link is-rounded is-outlined" on:click={goToNormal} id="btn1">Modalita Normale</button>
+    </div>
+
+    <div id="btn-htp">
+        <button class="button is-link is-rounded is-outlined" on:click={goToHTP} id="">Come si gioca</button>
+    </div>
+</main>
+  
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  
+    main{
+      font-family: 'Raleway', sans-serif;
+    }
+  
+    #title {
+      text-align: center;
+      margin-top: 50px;
+      font-weight: bold;
+    }
+
+    #btn{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 400px;
+    }
+    
+    #btn-htp{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 70px;
+    }
+  
+</style>
