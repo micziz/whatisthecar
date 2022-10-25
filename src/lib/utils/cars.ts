289ff485ab89738c19d409905827e0808655125a
micziz <miczicontent@gmail.com>
2022-10-25 11:19:14 +0200

    Better styiling in cars.ts

--- src/lib/utils/cars.ts
+++ src/lib/utils/cars.ts
@@ -1,1 +1,15 @@
-export let cars = ["audi", "chevrolet", "citroen", "honda", "hyundai", "pegout", "renault", "suzuki", "volkswagen", "toyota", "mercedes", "tesla"] 
+// Central cars array
+export let cars = [
+    "audi", 
+    "chevrolet", 
+    "citroen", 
+    "honda", 
+    "hyundai", 
+    "pegout", 
+    "renault", 
+    "suzuki", 
+    "volkswagen", 
+    "toyota", 
+    "mercedes", 
+    "tesla"
+] 
